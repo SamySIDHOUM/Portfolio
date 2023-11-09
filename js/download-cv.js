38e0f93f6915b5320c13cf523204360879040fcc
Samy SIDHOUM <samysidhoum6@gmail.com>
2023-11-09 14:03:57 +0100

    Ajout du script JS pour gérer le téléchargement du CV

---
+++ js/download-cv.js
@@ -0,0 +1,7 @@
+const downloadBtn = document.querySelector('.btn');
+
+// Ajout d'un gestionnaire d'événements pour le clic sur le bouton Télécharger CV
+downloadBtn.addEventListener('click', () => {
+    // Redirige l'utilisateur vers le fichier 
+    window.location.href = './assets/test.pdf';
+});
