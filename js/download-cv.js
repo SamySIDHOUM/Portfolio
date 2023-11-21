11514aa0c6c345ceecfb7d69e021626f21e7062d
Samy SIDHOUM <samysidhoum6@gmail.com>
2023-11-21 15:54:42 +0100

    Ajustement apporté au code

--- js/download-cv.js
+++ js/download-cv.js
@@ -3,5 +3,5 @@ const downloadBtn = document.querySelector('.btn');
 // Ajout d'un gestionnaire d'événements pour le clic sur le bouton Télécharger CV
 downloadBtn.addEventListener('click', () => {
     // Redirige l'utilisateur vers le fichier 
-    window.location.href = './assets/test.pdf';
+    window.location.href = './assets/CV_Samy_SIDHOUM.pdf';
 });
